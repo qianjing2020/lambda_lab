d0c79e94558da4d7fbc5d6609d181f2bfab22a9e
qianjing2020 <qianjing2020@users.noreply.github.com>
2020-06-01 21:52:29 -0400

    DQI added

--- models/db_connect.py
+++ models/db_connect.py
@@ -25,23 +25,28 @@ class dbConnect:
             "SELECT * FROM platform_market_prices2", con=engine)        
         return data
     
-
-    def populate_analytical_db(self, df, tablename):
-        # populate AWS analytical db 
-        # input df, tablename 
+    def read_analytical_db(self, tablename):
+        '''# read AWS analytical db 
         con = psycopg2.connect(
             user=aws_db_user,
             host=aws_db_host,
             port=aws_db_port,
             database=aws_db_name,
-            password=aws_db_password)
+            password=aws_db_password)'''
+        db_URI = 'postgresql://' + aws_db_user + ':' + aws_db_password + '@' + aws_db_host + '/' + aws_db_name
+        engine = create_engine(db_URI)
+        query_statement = "SELECT * FROM "+ tablename 
+        data = pd.read_sql(query_statement, con=engine)
+        return data
+
+    def populate_analytical_db(self, df, tablename):
+        # populate AWS analytical db 
+        # input df, tablename 
         db_URI = 'postgresql://' + aws_db_user + ':' + aws_db_password + '@' + aws_db_host + '/' + aws_db_name
         engine = create_engine(db_URI)
         
         df.to_sql(tablename, con=engine, if_exists='replace', index=False, chunksize=100)
-        con.commit()
-        con.close()
-
+       
     def migrate_analyticalDB(self):
         # read/add newly added data only
         pass #raw = read_stakeholderDB()
